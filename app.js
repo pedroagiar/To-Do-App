67803be97760474f80fb61c196865d66cd20548d
Pedro Aguiar <pedroaguiar.edu@gmail.com>
2022-07-18 22:35:46 -0300

    v3

--- app.js
+++ app.js
@@ -2,12 +2,13 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo');
 
 
 // Event Listeners
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
-
+filterOption.addEventListener('click', filterTodo);
 
 
 // Functions
@@ -61,3 +62,21 @@ function deleteCheck(e) {
     todo.classList.toggle("completed");
   }
 }
+
+
+function filterTodo(e) {
+  const todos = todoList.childNodes;
+  todos.forEach(function(todo) {
+    switch (e.target.value) {
+      case "all":
+        todo.style.display = 'flex';
+        break
+      case "completed":
+        if (todo.classList.contains('completed')) {
+          todo.style.display = 'flex';
+        } else {
+          todo.style.display = 'none';
+        }
+    };
+  });
+};
