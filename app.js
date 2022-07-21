8c2fb2585505184a459188372f875140c66407ef
Pedro Aguiar <pedroaguiar.edu@gmail.com>
2022-07-20 23:44:52 -0300

    Final Version 1.0

--- app.js
+++ app.js
@@ -149,4 +149,5 @@ function removeLocalTodos(todo){
   }
   const todoIndex = todo.children[0].innerText;
   todos.splice(todos.indexOf(todoIndex), 1);
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
